1acc1127d5cbbb8f6effe290eacac5ab9b4d1bd7
Kinjalc <kinjal.chauhan@gmail.com>
2015-07-27 00:48:16 -0400

    trying to fix bug - changes in serializer

--- app/Serializers/tourist_attraction_serializer.rb
+++ app/Serializers/tourist_attraction_serializer.rb
@@ -1,4 +1,4 @@
 class TouristAttractionSerializer < ActiveModel::Serializer
-  attributes :name, :description, :category, :id,:reviews_average
+  attributes :name, :description, :category, :id,:reviews_average,:latitude,:longitude
   has_many :reviews
 end
