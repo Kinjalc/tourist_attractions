ce4523228a6bf9d1bd92da58dd804b2679974c65
Kinjalc <kinjal.chauhan@gmail.com>
2015-07-27 00:22:20 -0400

    bug fix for map on loading page. HTMl css changes

--- public/app.js
+++ public/app.js
@@ -17,6 +17,7 @@ $(document).ready(function() {
     map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
   };
+  google.maps.event.addDomListener(window, 'load', initialize);
 
   function marker(map, pos) {
     var marker = new google.maps.Marker({
@@ -26,6 +27,7 @@ $(document).ready(function() {
     });
     map.panTo(position)
   };
+
 
   //Gets the list of cities from database
   $.ajax({
